f00c8a08f0ca62ea869eef4ed6182257d72fca30
valeriegonzalez <valerieggonzalez25@gmail.com>
2021-06-15 18:54:39 -0600

    Create Jefe.java
    trabajador equipamiento
    jefe

---
+++ proyectoPoo/src/main/java/modelo/Jefe.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author valerie
+ */
+public class Jefe {
+    public void brindarEquipamiento(int Trabajador trabajador) {
+        
+    }
+}
